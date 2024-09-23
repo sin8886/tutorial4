cbde116e28a30326c7da580d9ca4648f743d8c0f
sin8886 <113078181@qq.com>
2024-09-23 20:53:07 +0800

    Added test for subtraction

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -11,4 +11,9 @@ public class CalcTest {
         assertEquals(4, c.add(2,2));
     }
 
+    @Test
+    public void testSubtraction() {
+        assertEquals(2, c.subtract(4, 2)); // 期望结果为 2，实际结果为 c.subtract(4, 2)
+    }
 }
+
